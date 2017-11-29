e36fcf069f1ab49c6c6c5ad3c24e6fb455ae9187
cyij <cyij2006@gmail.com>
2017-11-29 12:41:17 +0800

    fix typo

--- controllers/Index.php
+++ controllers/Index.php
@@ -6,7 +6,7 @@ class Index extends Controller {
   public function render($path) {
     // set __SSR_INITIAL_STATE__ according to your business
     $this->templateValue('__SSR_INITIAL_STATE__', 1);
-    $this->show($path.'/index.html1');
+    $this->show($path.'/index.html');
   }
 
 }
