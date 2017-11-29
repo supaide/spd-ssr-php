f6431c0174138919fa69b6538fab78abadd3812c
cyij <cyij2006@gmail.com>
2017-11-29 23:48:35 +0800

    up

--- controllers/Index.php
+++ controllers/Index.php
@@ -3,10 +3,14 @@ include('Controller.php');
 
 class Index extends Controller {
 
+  protected function show($tpl) {
+    parent::show(LAYOUT_DIR . DIRECTORY_SEPARATOR . $tpl . DIRECTORY_SEPARATOR . 'index.html');
+  }
+
   public function render($path) {
     // set __SSR_INITIAL_STATE__ according to your business
     $this->templateValue('__SSR_INITIAL_STATE__', 1);
-    $this->show($path.'/index.html');
+    $this->show($path);
   }
 
 }
--- index.php
+++ index.php
@@ -3,13 +3,11 @@ ini_set('display_errors', false);
 error_reporting(E_ALL | E_STRICT);
 
 define('SITE_BASE', __DIR__);
+define('LAYOUT_DIR', SITE_BASE . DIRECTORY_SEPARATOR . 'views');
 include SITE_BASE . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
 $query = parse_url(filter_var(isset($_SERVER['REDIRECT_URL']) ? $_SERVER['REDIRECT_URL'] : $_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL));
 $path = trim($query['path'], '/');
-if (empty($path)) {
-  $path = 'index';
-}
 
 include SITE_BASE . DIRECTORY_SEPARATOR . 'controllers' . DIRECTORY_SEPARATOR . 'Index.php';
 
